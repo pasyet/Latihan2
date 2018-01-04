6134b9be4fae329c720e561823920b51c3ab2da8
pasyet <farapasyetnabila@gmail.com>
2018-01-04 16:57:03 +0700

    Latihan 2

--- JavaIndex.java
+++ JavaIndex.java
@@ -3,12 +3,12 @@ import java.lang.Math;
 public class JavaIndex {
       public static void main(String[] args) {
        int j,i;
-      for (i=0; i<=11; i++)  {
+      for (i=0; i<11; i++)  {
             for (j=0; j<5; j++){
                  if ((i%2)!=1){
-                  System.out.print(i*j +" ");
+                  System.out.print(i*j +"\t");
                  } else {
-                   System.out.print(Math.pow(i,j)+" ");
+                   System.out.print(Math.pow(i,j)+"\t");
                  }
                  
             }
