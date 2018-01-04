36625902ab70ac490496d12672e1df07629dea17
pasyet <farapasyetnabila@gmail.com>
2018-01-04 16:54:24 +0700

    Latihan 2

--- JavaBintang3.java
+++ JavaBintang3.java
@@ -1,13 +1,13 @@
 public class JavaBintang3 {
       public static void main(String[] args) {
           int j,i;
-      for (i=0; i<=6; i++)
+      for (i=1; i<=5; i++)
      {
-           for (j=0; j<i; j++)
+          for (j=5; j>=i; j--)
      {
-           System.out.print(""+i);
+           System.out.print(""+j);
      }
           System.out.println();
-  }    
+      }    
       }
 }
--- JavaIndex.java
+++ JavaIndex.java
@@ -1,5 +1,19 @@
+import java.lang.Math; 
+
 public class JavaIndex {
       public static void main(String[] args) {
-          
+       int j,i;
+      for (i=0; i<=11; i++)  {
+            for (j=0; j<5; j++){
+                 if ((i%2)!=1){
+                  System.out.print(i*j +" ");
+                 } else {
+                   System.out.print(Math.pow(i,j)+" ");
+                 }
+                 
+            }
+      System.out.println();
       }
+
+}
 }
