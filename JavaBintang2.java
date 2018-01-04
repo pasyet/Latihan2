7e78e15e19dd513b8d0a27ef8c55226e09165400
pasyet <farapasyetnabila@gmail.com>
2018-01-04 14:20:54 +0700

    Latihan 2

---
+++ JavaBintang2.java
@@ -0,0 +1,13 @@
+public class JavaBintang2 {
+      public static void main(String[] args) {
+        int j,i;
+      for (i=0; i<=6; i++)
+     {
+           for (j=1; j<i; j++)
+     {
+           System.out.print("*");
+     }
+          System.out.println();
+  }
+    }
+}
